936d0c0c103a5fe2af77a8347947bc41404a6013
Atte Perämäki <atte-90@gmail.com>
2024-12-20 00:51:24 +0200

    Day 05 init

---
+++ day05/src/main.rs
@@ -0,0 +1,34 @@
+
+fn solve_part1(input: &str)-> u32 {
+    todo!();
+}
+
+fn solve_part2(input: &str) -> u32 {
+    todo!();
+}
+
+fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
+
+    let input = include_str!("../input.txt");
+    let result = solve_part1(input);
+    println!(
+        "Part 1 solution: {}, time taken {:.2?}",
+        result,
+        now.elapsed()
+    );
+
+    let now = Instant::now();
+    let result = solve_part2(input);
+    println!(
+        "Part 2 solution: {}, time taken {:.2?}",
+        result,
+        now.elapsed()
+    );
+}
+
+
+#[cfg(test)]
+mod tests;
+
