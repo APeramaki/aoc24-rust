dec628e209e8c2654e28cd87d762b03f8d04a3a1
Atte Perämäki <atte-90@gmail.com>
2024-12-19 13:13:10 +0200

    day03 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -9,8 +9,32 @@ fn solve_part1(input: &str) -> u32 {
                 .find_iter(mul.as_str())
                 .map(|nums| nums.as_str().parse::<u32>().unwrap())
                 .product::<u32>()
-        }).sum::<u32>()
-    
+        })
+        .sum::<u32>()
+}
+
+fn solve_part2(input: &str) -> u32 {
+    let regex = Regex::new(r"(don't\(\))|(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))").unwrap();
+    let num_regex = Regex::new(r"[0-9]{1,3}").unwrap();
+    let mut mul_enabled = true;
+    regex
+        .find_iter(input)
+        .map(|needle| match needle.as_str() {
+            "do()" => {
+                mul_enabled = true;
+                0
+            }
+            "don't()" => {
+                mul_enabled = false;
+                0
+            }
+            _ if mul_enabled => num_regex
+                .find_iter(needle.as_str())
+                .map(|needle| needle.as_str().parse::<u32>().unwrap())
+                .product::<u32>(),
+            _ => return 0,
+        })
+        .sum::<u32>()
 }
 
 fn main() {
@@ -25,9 +49,13 @@ fn main() {
         now.elapsed()
     );
 
-    // let now = Instant::now();
-    // let result = solve_part2(input);
-    // println!("Part 2 solution: {}, time taken {:.2?}", result, now.elapsed());
+    let now = Instant::now();
+    let result = solve_part2(input);
+    println!(
+        "Part 2 solution: {}, time taken {:.2?}",
+        result,
+        now.elapsed()
+    );
 }
 
 #[cfg(test)]
--- day03/src/tests.rs
+++ day03/src/tests.rs
@@ -9,6 +9,6 @@ fn part1_safe_count() {
 #[test]
 fn part2_safe_count() {
   let example_input =
-  "";
-  assert_eq!(1,1);
+  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+  assert_eq!(solve_part2(example_input),48);
 }
