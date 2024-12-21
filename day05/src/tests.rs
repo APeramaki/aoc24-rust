9a5a1c978c18613dc75ae000e4e5c07d75a9e587
Atte Perämäki <atte-90@gmail.com>
2024-12-21 14:32:43 +0200

    day 05 part 1

--- day05/src/tests.rs
+++ day05/src/tests.rs
@@ -1,9 +1,8 @@
 use super::*;
 #[test]
 fn part1() {
-    let example_input =
-    "
-    47|53
+    let example_input = 
+    "47|53
     97|13
     97|61
     97|47
@@ -31,14 +30,13 @@ fn part1() {
     75,97,47,61,53
     61,13,29
     97,13,75,29,47";
-    assert_eq!(solve_part1(example_input), 18);
+    assert_eq!(solve_part1(example_input), 143);
 }
 
 #[test]
 fn part2() {
   let example_input =
-    "
-    47|53
+    "47|53
     97|13
     97|61
     97|47
